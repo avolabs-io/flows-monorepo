74235044d04fa7669db6c98da5c3744f9b5eea2e
denhampreen <denhampreen@gmail.com>
2021-01-12 10:54:15 +0200

    docusaurus boilerplate & configuration

---
+++ site/README.md
@@ -0,0 +1,23 @@
+# Flows.finance site
+
+This directory hosts the flows.finance public facing website and documentation
+The site is built using [docusaurus](https://v2.docusaurus.io/) in React
+
+## Local Development
+
+```console
+yarn
+yarn start
+```
+
+## Build
+
+```console
+yarn build
+```
+
+## Deployment
+
+```console
+GIT_USER=<Your GitHub username> USE_SSH=true yarn deploy
+```
---
+++ site/docusaurus.config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  title: "Flows Finance",
+  tagline: "Privacy preserving continuous payment streams",
+  url: "https://flows.finance",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon/favicon.ico",
+  organizationName: "avolabs-io",
+  projectName: "Flows.finance",
+  themeConfig: {
+    navbar: {
+      title: "Flows Finance",
+      logo: {
+        alt: "Flows Finance logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
+        },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "#",
+          label: "Discord",
+          position: "right",
+        },
+        {
+          href: "#",
+          label: "Twitter",
+          position: "right",
+        },
+        {
+          href: "#",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/",
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/flows_finance",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/flows_finance",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Flows.Finance`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/avolabs-io/flows-monorepo/",
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: "https://github.com/avolabs-io/flows-monorepo/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
