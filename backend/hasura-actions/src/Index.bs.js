ec627a3c511d17a0f59f728b740bace669b49c34
Wyn <wynrdavies@gmail.com>
2021-01-26 11:40:55 +0200

    Basic user auth. (BE-94846)
    Co-authored-by: Jason Smythe <JasoonS@users.noreply.github.com>

--- backend/hasura-actions/src/Index.bs.js
+++ backend/hasura-actions/src/Index.bs.js
@@ -3,6 +3,7 @@
 
 var Serbet = require("serbet/src/Serbet.bs.js");
 var Dotenv = require("dotenv");
+var AuthHook = require("./AuthHook.bs.js");
 var Pervasives = require("bs-platform/lib/js/pervasives.js");
 var Belt_Option = require("bs-platform/lib/js/belt_Option.js");
 var PaymentStreamManager = require("./PaymentStreamManager.bs.js");
@@ -11,7 +12,10 @@ Dotenv.config();
 
 var app = Serbet.application(Belt_Option.getWithDefault(Belt_Option.flatMap(process.env.PORT, Pervasives.int_of_string_opt), 9898), {
       hd: PaymentStreamManager.createStream,
-      tl: /* [] */0
+      tl: {
+        hd: AuthHook.endpoint,
+        tl: /* [] */0
+      }
     });
 
 exports.app = app;
